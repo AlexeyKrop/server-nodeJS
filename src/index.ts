076553798a167b90251bc431411fcff8a2c6cefa
EnzoK <kropotin_aleksey@mail.ru>
2022-10-20 07:18:35 +0300

    feat: add get request

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-import express, { Request, Response } from "express";
+import express, {Request, Response} from "express";
+
 const app = express();
-const port = 3001
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello world');
+const port = process.env.PORT || 3001
+const products = [
+  {title: 'tomato'}, {title: 'apple'}
+]
+app.get('/products', (req: Request, res: Response) => {
+  res.send(products);
+});
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+  const product = products.find((product) => product.title === req.params.productTitle)
+  if(product){
+    res.send(product);
+  }else{
+    res.send(404)
+  }
+
 });
 
 app.listen(port, () => {
