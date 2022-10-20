2aabd5de3a0e51393a3eac189333bff127b3c3f4
EnzoK <kropotin_aleksey@mail.ru>
2022-10-20 07:37:13 +0300

    feat: add get request with query params

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,20 @@ const products = [
   {title: 'tomato'}, {title: 'apple'}
 ]
 app.get('/products', (req: Request, res: Response) => {
+  if (req.query.title) {
+    const searchStr = req.query.title.toString()
+    const product = products.filter(product => product.title.indexOf(searchStr) > -1)
+    res.send(product)
+  }
   res.send(products);
 });
 app.get('/products/:productTitle', (req: Request, res: Response) => {
   const product = products.find((product) => product.title === req.params.productTitle)
-  if(product){
+  if (product) {
     res.send(product);
-  }else{
+  } else {
     res.send(404)
   }
-
 });
 
 app.listen(port, () => {
