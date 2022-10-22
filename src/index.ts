7ac43e9e99d7e267d13ef135bda8efde1ea1bd1b
EnzoK <kropotin_aleksey@mail.ru>
2022-10-22 18:56:08 +0300

    feat: add db.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from 'body-parser'
 import {productsRouter} from "./routes/products-routes/products-router";
-
+import {runDb} from "./dal/db";
 const app = express();
 const port = process.env.PORT || 3000
 
@@ -9,11 +9,12 @@ const parserMiddleWare = bodyParser({})
 app.use(parserMiddleWare)
 
 app.use('/products', productsRouter)
-try {
+
+const startApp = async () => {
+  await runDb()
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-} catch (e) {
-  console.log(e)
 }
+startApp()
 
